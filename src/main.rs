d8562559e679636f35e2b27b4e1502175d1a32f1
IainLogie <iainlogie@eircom.net>
2024-05-28 20:30:45 +0100

    Updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    let apples = 5; // immutable
+    let mut bananas = 5; // mutable
+    bananas = bananas + 1;
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
+    println!("Apples: {apples}");
+    println!("Bananas: {bananas}");
+
+    let x = 5;
+    let y = 10;
+    
+    println!("x = {x} and y + 2 = {}", y + 2);
+
 }
